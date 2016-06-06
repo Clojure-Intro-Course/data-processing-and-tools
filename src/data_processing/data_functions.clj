4b814a29929cf6752794628a3e1b4d1f09a2d628
Tony Song <songx823@morris.umn.edu>
2016-06-06 13:25:58 -0500

    added documentation to undocumented functions

--- src/data_processing/data_functions.clj
+++ src/data_processing/data_functions.clj
@@ -1,6 +1,22 @@
 (ns data_processing.data_functions
   (:use data_processing.data))
 
+
+
+(defn get-map
+  [person th]
+  "gets the map of nth vector"
+  (nth person (+ (* th 2) 1)))
+
+(defn get-key [person th]
+  "gets the key (string) of nth vector"
+  (nth person (* th 2)))
+
+(defn time->seconds [h-map]
+  "takes the map of a question in a person structure
+  and returns the converted time in seconds"
+  (+ (* (:min h-map) 60) (:sec h-map)))
+
 (defn vector->map
   [input-vector]
   "converts a vector into a map"
@@ -24,35 +40,27 @@
   "takes a person as an argument
   and computes the total time in seconds that the person takes for the test after adjustment"
   (let [last-entry (last person)]
-    (+ (* (:min last-entry) 60) (:sec last-entry) (time-adj-result person))))
+    (+ (time->seconds last-entry) (time-adj-result person))))
 
 ; top level function
-(defn total-time [person]
+(defn total-time
+  [person]
   "takes a person as an argument
   and returns a nice looking string in the form \"XX minutes XX seconds\""
   (str (quot (after-time-adj person) 60) " minutes " (mod (after-time-adj person) 60) " seconds"))
 
 ; top level function
-(defn solved-questions [person]
+(defn solved-questions
+  [person]
   "takes a person as an argument
   and returns the number of questions the person solved"
   (count (filter (fn [question] (:solved question)) (vals (vector->map person)))))
 
-(defn get-question [person question]
-  ((apply hash-map person) question))
-
-(defn get-map [person th]
-  (nth person (+ (* th 2) 1)))
-
-(defn get-key [person th]
-  (nth person (* th 2)))
-
-(defn time->seconds [h-map]
-  (+ (* (:min h-map) 60) (:sec h-map)))
-
-; 1 is Racket 2 is Clojure
-(defn language-check [person]
-  (if (= (first (get-key person 0)) \R) 1 2))
+(defn lang?
+  [s]
+  "takes a String
+  and returns 0 if it represents racket 1 if it represents clojure"
+  (if (= (first s) \R) 0 1))
 
 (defn nice-time [seconds]
   (str (quot seconds 60) "min " (mod seconds 60) "sec"))
@@ -77,8 +85,7 @@
 ; for each repeated question
 ;    - find the location
 ;    - update it by assoc-in
-(defn lang? [s]
-  (if (= (first s) \R) 0 1)) ;racket 0 clojure 1
+
 
 (defn normal [person]
   (vec (filter (fn [each-vec] (<= (count (first each-vec)) 5)) (take-q-info person))))
@@ -108,8 +115,23 @@
 
 ; need a function that takes a vector of data points and print all the data
 
-; ugly code for printing
-(println "|Name\t\t\t|Total Time\t\t|Solved Questions/Number of Questions")
-(println)
-(printf "Someone-Racket\t\t%s\t%s\n" (total-time person) (str (solved-questions person) "/" 8))
-(println (adjusted-data person))
+;(println "|Name\t\t\t|Total Time\t\t|Solved Questions/Number of Questions")
+;(println)
+;(printf "Someone-Racket\t\t%s\t%s\n" (total-time person) (str (solved-questions person) "/" 8))
+;(println (adjusted-data person))
+
+
+(defn printing-result [p-vec]
+  (when ((complement empty?) p-vec)
+    (let [head (first p-vec)]
+      (printf "Someone-Racket\t\t%s\t%s\n" (total-time head) (str (solved-questions head) "/" 8))
+      (println (adjusted-data head))
+      (println)
+      (printing-result (rest p-vec)))))
+
+(defn print-result [p-vec]
+  (println "|Name\t\t\t|Total Time\t\t|Solved Questions/Number of Questions\n")
+  (printing-result p-vec)
+  (println "===================================END========================================"))
+
+(print-result qTables)
